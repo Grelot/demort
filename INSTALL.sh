2b3c021adabaadabc043a4e65737ab85616f7323
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-31 14:52:40 +0200

    install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,34 +1,15 @@
-## python3
+###############################################################################
+## python3-packages
+#### biopython
+python3 -m pip install biopython
+#### joblib 
+python3 -m pip install joblib
+#### numpy
+python3 -m pip install numpy
+#### matplotlib
+python3 -m pip install matplotlib
 
-## packages
-
-## biopython
-python3.7 -m pip install biopython
-## argparse
-
-## joblib 
-python3.7 -m pip install joblib
-## multiprocessing
-python3.7 -m pip install joblib
-
-
-## numpy
-python3.7 -m pip install numpy
-
-
-## matplotlib
-python3.7 -m pip install matplotlib
-
-## deprecated
-
-## plotly
-#pip3 install plotly==4.0.0
-## chart studio
-#pip3 install chart-studio
-## tornado
-#pip3 install "tornado==5"
-
-
+###############################################################################
 ## NUITKA
 ## Stable: Debian/Ubuntu/Mint repositories
 
