58de5e6b50e59f4efe56e67c16268d1765c4484c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-31 14:53:42 +0200

    PyPI setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("doc/pypi_readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='demort',  
+
+     version='0.2.2',
+
+     scripts=['src/demort.py'] ,
+
+     author="Pierre-Edouard GUERIN",
+
+     author_email="pierre-edouard.guerin@cefe.cnrs.fr",
+
+     description="DEmultiplexing MOnitoring Report Tool",
+
+     long_description=long_description,
+     long_description_content_type='text/markdown', 
+
+
+
+     url="https://github.com/Grelot/demort",
+     packages=setuptools.find_packages(),
+
+    install_requires=['argparse', 'numpy', 'matplotlib', 'biopython', 'joblib'],
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+)
